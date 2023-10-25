4b28540145b6f90ef7c577efdb5b1cfa4bd44858
euger <azkriven17@gmail.com>
2023-10-25 15:58:39 +0800

    added NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en" suppressHydrationWarning>
         <body className={cn("antialiased", inter.className)}>
           <ThemeProvider
