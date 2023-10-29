8efb00150a43df3b3e5a6bf5183b27c9009b055e
euger <azkriven17@gmail.com>
2023-10-29 18:19:13 +0800

    added textarea to comments page

--- components/anime/comments.tsx
+++ components/anime/comments.tsx
@@ -7,6 +7,8 @@ import { Trash } from "lucide-react";
 import { toast } from "sonner";
 import { format } from "date-fns";
 import AddComment from "./add-comment";
+import Image from "next/image";
+import { Textarea } from "../ui/textarea";
 
 export default function Comments() {
   const searchParams = useSearchParams();
@@ -43,27 +45,62 @@ export default function Comments() {
         <h2 className="text-lg md:text-xl font-semibold">Comments</h2>
         {user && <AddComment />}
         {allComments?.length ? (
-          allComments.reverse().map((comment) => (
+          allComments.map((comment) => (
+            // <div
+            //   key={comment.id}
+            //   className="mt-4 p-4 border-t border flex items-center gap-2 w-full justify-between"
+            // >
+            //   <div className="flex gap-5 items-center">
+            //     <Image
+            //       src={comment.userImg}
+            //       alt="User Avatar"
+            //       height={35}
+            //       width={35}
+            //       className="rounded-full object-cover"
+            //     />
+            //     <div className="max-h-52 overflow-y-scroll whitespace-normal overflow-x-hidden pr-2">
+            //       <p className="font-semibold">{comment.userName}</p>
+
+            //       <p className="text-muted-foreground text-xs">
+            //         {format(new Date(comment.createdAt), "MMM d, yyy")}
+            //       </p>
+            //     </div>
+            //   </div>
+            //   {user?.id === comment.userId && (
+            //     <Button
+            //       variant="destructive"
+            //       size="icon"
+            //       className="min-w-[40px]"
+            //     >
+            //       <Trash
+            //         onClick={() =>
+            //           deleteComment({
+            //             episodeId: episode,
+            //             comment: comment.comment,
+            //           })
+            //         }
+            //         className="w-4 h-4"
+            //       />
+            //     </Button>
+            //   )}
+            // </div>
             <div
               key={comment.id}
-              className="mt-4 p-4 border-t border flex items-center gap-2 w-full justify-between"
+              className="flex justify-between items-center my-5 gap-2"
             >
-              <div className="flex gap-5">
-                <img
-                  src={comment.userImg}
-                  alt="User Avatar"
-                  className="w-10 h-10 rounded-full"
-                />
-                <div className="max-h-52 overflow-y-scroll pr-2">
-                  <p className="font-semibold">{comment.userName}</p>
-                  <p className="text-muted-foreground text-sm">
-                    {comment.comment}
-                  </p>
-                  <p className="text-muted-foreground text-xs">
-                    {format(new Date(comment.createdAt), "MMM d, yyy")}
-                  </p>
-                </div>
-              </div>
+              <Image
+                src={comment.userImg}
+                alt="User Avatar"
+                height={35}
+                width={35}
+                className="rounded-full object-cover"
+              />
+              <Textarea
+                readOnly
+                className="text-sm text-muted-foreground resize-none"
+              >
+                {comment.comment}
+              </Textarea>
               {user?.id === comment.userId && (
                 <Button
                   variant="destructive"
